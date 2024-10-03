ca0c8642f7f3dbbda9907e50d1143b1f3b0aca88
Mariia Chedryk <mary.chedryk@gmail.com>
2024-10-03 13:33:13 -0400

    first commit

---
+++ JS/main.js
@@ -0,0 +1,40 @@
+let age;
+age = 14;
+console.log(age);
+
+let name;
+name = "Mariia"
+console.log(name);
+
+let isStudent;
+isStudent = "true"
+console.log(isStudent);
+
+let myString;
+myString = "True friendship come when silence between the people is comfortable"
+console.log(myString);
+
+let myNumber;
+myNumber = 21;
+let added = myNumber + 10;
+console.log(myNumber);
+console.log(added);
+
+let myNull;
+myNull = null;
+console.log(myNull);
+
+const data = prompt("Enter your Name");
+
+console.log(data);
+
+if (data) {
+   confirm("Thanks for confirmation")
+}
+else {
+   confirm("Action canceled")
+}
+
+const message = "Action is dangerous. Confirm it before moving on.";
+
+alert(message);
